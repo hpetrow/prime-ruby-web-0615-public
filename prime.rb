cee5a153e02a224f7de2bf6edc0de5b0ef2df0ed
hpetrow <heather.petrow@gmail.com>
2015-06-04 15:23:41 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
 # Add  code here!
+require 'benchmark'
+
+def prime? (n)
+  if (n <= 0)
+    result = false
+  elsif (n ==1 || n == 2)
+    result = true
+  else
+    primes = sieve_of_eras(n)
+    result = primes.last == n ? true : false
+  end
+  result
+end
+
+def sieve_of_eras (n)
+  primes = (3..n).to_a
+  primes = primes.select {|n| n == 2 || n.odd?}
+  primes.each {|prime|
+    break if prime**2 > primes.last
+    primes = primes.select {|n| n == prime || n%prime != 0}
+  }
+  primes
+end
+
+Benchmark.bm do |bm|
+  bm.report {prime?(1000)}
+end
